09fbf02f639ba9857c58e6c465543e51900ccc71
Matthew Chipperfield <58623934+GoaferLX@users.noreply.github.com>
2020-12-19 22:20:22 +0000

    correctly returning errors

--- game/character/database.go
+++ game/character/database.go
@@ -3,8 +3,8 @@ package character
 import "database/sql"
 
 type character struct {
-	id uint64
-	balance uint64
+	id         uint64
+	balance    uint64
 	reputation int16
 }
 
@@ -12,12 +12,12 @@ func getCharacter(db *sql.DB, id uint64) (character, error) {
 	row := db.QueryRow("SELECT * FROM characters WHERE id = $1", id)
 
 	if err := row.Err(); err != nil {
-		return character{}, nil
+		return character{}, err
 	}
 
 	var c character
 	if err := row.Scan(&c.id, &c.balance, &c.reputation); err != nil {
-		return character{}, nil
+		return character{}, err
 	}
 
 	return c, nil
